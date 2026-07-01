251504a140ef712cc88a13e06c865aca4f6dace6
Aseem Krishna Choudhary <aseemkrishnac@gmail.com>
2026-07-01 10:55:54 +0530

    Over-riding the Methods

---
+++ Python_basics/Method_Overriding.py
@@ -0,0 +1,17 @@
+class Shape:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def area(self):
+        return self.x * self.y
+
+class Circle(Shape):
+    def  __init__(self, radius):
+        self.radius = radius
+        super().__init__(radius, radius)
+    def area(self):
+        return 3.14 * super().area()
+rec = Shape(5, 3)
+print(rec.area())
+circ = Circle(5)
+print(circ.area())
