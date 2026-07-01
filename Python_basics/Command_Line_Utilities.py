54e9713d172b1940a351505c6cd6bdaeea5956f6
Aseem Krishna Choudhary <aseemkrishnac@gmail.com>
2026-07-01 13:50:48 +0530

    Just to change the filename

--- Python_basics/Command_Line_Utilities.py
+++ Python_basics/Command_Line_Utilities.py
@@ -2,7 +2,6 @@ import argparse
 import requests
 
 def DownloadFile(url, local_filename):
-    local_filename = url.split('/')[-1]
     r = requests.get(url)
     f = open(local_filename, 'wb')
     for chunk in r.iter_content(chunk_size=512 * 1024):
