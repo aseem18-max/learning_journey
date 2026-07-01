969c69138a662a3e9b165097081b662a07169293
Aseem Krishna Choudhary <aseemkrishnac@gmail.com>
2026-07-01 13:47:00 +0530

    Argparse Module

---
+++ Python_basics/Command_Line_Utilities.py
@@ -0,0 +1,25 @@
+import argparse
+import requests
+
+def DownloadFile(url, local_filename):
+    local_filename = url.split('/')[-1]
+    r = requests.get(url)
+    f = open(local_filename, 'wb')
+    for chunk in r.iter_content(chunk_size=512 * 1024):
+        if chunk: # filter out keep-alive new chunks
+            f.write(chunk)
+    f.close()
+    return
+
+
+parser = argparse.ArgumentParser()
+
+# Add Command line arguments
+parser.add_argument("url", help = "Url of the file to download")
+parser.add_argument("output", help = "by which name do you to save your file")
+
+args = parser.parse_args()
+
+print(args.url)
+print(args.output)
+DownloadFile(args.url, args.output)
