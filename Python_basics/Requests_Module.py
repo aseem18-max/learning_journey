49861307bd1eac17257be64593a22bb05b69850c
Aseem Krishna Choudhary <aseemkrishnac@gmail.com>
2026-07-02 12:48:34 +0530

    Requests

--- Python_basics/Requests_Module.py
+++ Python_basics/Requests_Module.py
@@ -3,6 +3,6 @@ from bs4 import BeautifulSoup
 url = "https://www.codewithharry.com/blogpost/50-linux-commands"
 r = requests.get(url)
 soup = BeautifulSoup(r.text, 'html.parser')
-#print(soup.prettify())
+print(soup.prettify())
 for heading in soup.find_all("h2"):
     print(heading.text)
